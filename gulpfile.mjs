1ad5e7dba5f19912eb5e959a79906e70c0d3e3ca
ChristopherSD <christopherDick00@gmail.com>
2023-09-27 23:27:54 +0200

    Add gulp tasks for css compiling
    Simplify the folder structure of styles. Rename the folder to "less" to make it clearer.
    
    Introduce package.json for dev dependencies.

---
+++ gulpfile.mjs
@@ -0,0 +1,22 @@
+import gulp from "gulp";
+
+import * as css from "./utils/css.mjs";
+import * as javascript from "./utils/javascript.mjs";
+
+
+// default export - build CSS and watch for updates
+export default gulp.series(
+    gulp.parallel(css.compile),
+    css.watchUpdates
+);
+
+// CSS compiling
+export const buildCSS = gulp.series(css.compile);
+
+// javascript linting
+// export const lint = gulp.series(javascript.lint);
+
+// build all artifacts
+export const buildAll = gulp.parallel(
+    css.compile
+);
