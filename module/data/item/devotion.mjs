b6764d26bce2e38df29ed64da4f00833f5df58fc
patrickmohrmann <76265425+patrickmohrmann@users.noreply.github.com>
2023-10-07 21:59:09 +0200

    Update devotion.mjs
    typo

--- module/data/item/devotion.mjs
+++ module/data/item/devotion.mjs
@@ -1,10 +1,9 @@
-
 import AbilityTemplate from "./templates/ability.mjs";
 
 /**
  * Data model template with information on Devotion items.
  */
-export default class DevotionData extends AbilityTemplate{
+export default class DevotionData extends AbilityTemplate {
 
     /** @inheritDoc */
     static defineSchema() {
