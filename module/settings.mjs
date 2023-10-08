f84320dc61a10f37f805c9542ead4f8f0931da87
patrickmohrmann <patrick.mohrmann@outlook.de>
2023-10-09 00:33:10 +0200

    added StepTable system settings

--- module/settings.mjs
+++ module/settings.mjs
@@ -1,4 +1,20 @@
 /**
  * Register all the system's settings.
  */
-export default function registerSystemSettings() {}
+export default function registerSystemSettings() {
+    // Step Table used for step to dice conversion
+    game.settings.register("ED4E", "stepTable", {
+        name: "Settings.StepTable.stepTable",
+        hint: "Settings.StepTable.hint",
+        scope: "world",
+        config: true,
+        default: "fourth",
+        type: String,
+        choices: {
+            classic: "Settings.StepTable.editionClassic",
+            first: "Settings.StepTable.editionfirst",
+            third: "Settings.StepTable.editionThird",
+            fourth: "Settings.StepTable.editionFourth"
+          }
+    });
+}
