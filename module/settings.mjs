9faa5957e9495226e68eaf2e7823ee3f96d16967
patrickmohrmann <patrick.mohrmann@outlook.de>
2023-10-09 12:55:19 +0200

    added dark modus System setting
    including lang file update (de)

--- module/settings.mjs
+++ module/settings.mjs
@@ -1,20 +1,38 @@
 /**
  * Register all the system's settings.
  */
-export default function registerSystemSettings() {
+export async function registerSystemSettings() {
     // Step Table used for step to dice conversion
-    game.settings.register("ED4E", "stepTable", {
-        name: "Settings.StepTable.stepTable",
-        hint: "Settings.StepTable.hint",
+    game.settings.register("ed4e", "stepTable", {
+        name: "ED.Settings.StepTable.stepTable",
+        hint: "ED.Settings.StepTable.hint",
         scope: "world",
         config: true,
         default: "fourth",
         type: String,
         choices: {
-            classic: "Settings.StepTable.editionClassic",
-            first: "Settings.StepTable.editionfirst",
-            third: "Settings.StepTable.editionThird",
-            fourth: "Settings.StepTable.editionFourth"
+            classic: "ED.Settings.StepTable.editionClassic",
+            first: "ED.Settings.StepTable.editionfirst",
+            third: "ED.Settings.StepTable.editionThird",
+            fourth: "ED.Settings.StepTable.editionFourth"
           }
+    });
+
+    // dark mode for sheets
+    game.settings.register("ed4e", "darkMode", {
+        name: "ED.Settings.DarkMode.darkMode",
+        hint: "ED.Settings.DarkMode.hint",
+        scope: "client",
+        config: true,
+        default: 50,
+        type: Number,
+        range: {
+            min: 0,
+            max: 100,
+            step: 5
+        },
+        onChange: async(val) => {
+            // change the darkness level to = val
+        }
     });
 }
