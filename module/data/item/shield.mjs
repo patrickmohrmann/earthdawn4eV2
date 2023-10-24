e775592ddeb85096cd5101a3243650ea46923def
patrickmohrmann <patrick.mohrmann@outlook.de>
2023-10-24 17:03:09 +0200

    shield data model

--- module/data/item/shield.mjs
+++ module/data/item/shield.mjs
@@ -7,16 +7,64 @@ export default class ShieldData extends PhysicalItemTemplate{
 
     /** @inheritDoc */
     static defineSchema() {
-        return {};
+        return this.mergeSchema( super.defineSchema(), {
+            defenseBonusPhysical: new foundry.data.fields.NumberField( {
+                required: true,
+                nullable: false,
+                min: 0,
+                initial: 0,
+                positive: true,
+                hint: "ED.Item.Shield.Label.defenseBonusPhysical",
+                label: "ED.Item.Shield.Hint.defenseBonusPhysical"
+            } ),
+            defenseBonusMystical: new foundry.data.fields.NumberField( {
+                required: true,
+                nullable: false,
+                min: 0,
+                initial: 0,
+                positive: true,
+                hint: "ED.Item.Shield.Label.defenseBonusMystical",
+                label: "ED.Item.Shield.Hint.defenseBonusMystical"
+            } ),
+            initiativePenalty: new foundry.data.fields.NumberField( {
+                required: true,
+                nullable: false,
+                min: 0,
+                initial: 0,
+                positive: true,
+                hint: "ED.Item.Shield.Label.initiativePenalty",
+                label: "ED.Item.Shield.Hint.initiativePenalty"
+            } ),
+            shatterThreshold: new foundry.data.fields.NumberField( {
+                required: true,
+                nullable: false,
+                min: 0,
+                initial: 0,
+                positive: true,
+                hint: "ED.Item.Shield.Label.shatterThreshold",
+                label: "ED.Item.Shield.Hint.shatterThreshold"
+            } ),
+    } );
     }
+
+    // "templatesItem": [
+    //     "itemBasics",
+    //     "equipmentBasics",
+    //     "detailsEquipment",
+    //     "detailsGeneral"
+    // ],
+    // "defenseBonusPhysical": 0,
+    // "defenseBonusMystical": 0,
+    // "initiativePenalty": 0,
+    // "shatterThreshold": 0
 
     /* -------------------------------------------- */
     /*  Migrations                                  */
     /* -------------------------------------------- */
 
     /** @inheritDoc */
-    static migrateData(source) {
-        super.migrateData(source);
+    static migrateData( source ) {
+        super.migrateData( source );
         // specific migration functions
     }
 }
