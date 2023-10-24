18c880a6d64569944c28e7f128cb1daf1d893611
patrickmohrmann <patrick.mohrmann@outlook.de>
2023-10-25 00:18:30 +0200

    corror mark / curse data tempalte

--- module/data/item/curse-horror-mark.mjs
+++ module/data/item/curse-horror-mark.mjs
@@ -2,12 +2,39 @@ import SystemDataModel from "../abstract.mjs";
 
 /**
  * Data model template with information on Curse and Horror Mark items.
+ * @property {} curseStep           curse step
+ * @property {} curseType           type of the curse
+ * @property {} curseActive         is the curse active
+ * @property {} curseDetected       is the curse known
  */
 export default class CurseHorrorMarkData extends SystemDataModel {
 
     /** @inheritDoc */
     static defineSchema() {
-        return {};
+        return this.mergeSchema( super.defineSchema(), {
+            curseStep: new foundry.data.fields.NumberField( {
+                required: true,
+                nullable: false,
+                min: 0,
+                initial: 0,
+                label: "ED.Item.Curse.curseStep"
+            } ), 
+            curseType: new foundry.data.fields.StringField( {
+                required: true,
+                blank: false,
+                initial: "Silver",
+                label: "ED.Item.Curse.curseType"
+            } ),
+            curseActive: new foundry.data.fields.BooleanField( {
+                required: true,
+                label: "ED.Item.Curse.curseActive"
+            } ),
+            curseDetected: new foundry.data.fields.BooleanField( {
+                required: true,
+                initial: false,
+                label: "ED.Item.Curse.curseDetected"
+            } ),
+        } );
     }
 
     /* -------------------------------------------- */
@@ -15,8 +42,8 @@ export default class CurseHorrorMarkData extends SystemDataModel {
     /* -------------------------------------------- */
 
     /** @inheritDoc */
-    static migrateData(source) {
-        super.migrateData(source);
+    static migrateData( source ) {
+        super.migrateData( source );
         // specific migration functions
     }
 }
