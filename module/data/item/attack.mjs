6480289f3141b18fd99e1fd0c2bc667a481792fe
patrickmohrmann <patrick.mohrmann@outlook.de>
2023-10-24 23:43:21 +0200

    Update attack.mjs
    changed attack form abilityDataModel to SystemDataModel

--- module/data/item/attack.mjs
+++ module/data/item/attack.mjs
@@ -1,15 +1,29 @@
-import AbilityTemplate from "./templates/ability.mjs";
+import SystemDataModel from "../abstract.mjs";
 
 /**
  * Data model template with information on Attack items.
  * @property {number} attackStep
  * @property {number} damageStep
  */
-export default class AttackData extends AbilityTemplate {
+export default class AttackData extends SystemDataModel {
 
     /** @inheritDoc */
     static defineSchema() {
         return this.mergeSchema( super.defineSchema(), {
+            action: new foundry.data.fields.StringField( {
+                required: true,
+                nullable: false,
+                blank: false,
+                initial: "standard",
+                label: "ED.Item.ability.action"
+            } ), 
+            strain: new foundry.data.fields.NumberField( {
+                required: true,
+                nullable: false,
+                min: 0,
+                initial: 0,
+                label: "ED.Item.ability.strain"
+            } ),
             attackStep: new foundry.data.fields.NumberField( {
                 required: true,
                 nullable: false,
