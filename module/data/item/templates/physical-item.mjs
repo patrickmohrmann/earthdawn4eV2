1d9b0b2b9c78a1094f8d02991d1a727a4c3635b4
patrickmohrmann <patrick.mohrmann@outlook.de>
2023-10-24 10:36:51 +0200

    physical item + lang

--- module/data/item/templates/physical-item.mjs
+++ module/data/item/templates/physical-item.mjs
@@ -2,12 +2,74 @@ import SystemDataModel from "../../abstract.mjs";
 
 /**
  * Data model template with information on physical items.
+ * @property {object} price 
+ * @property {number} price.value item cost 
+ * @property {string} price.coinage coinage type of the cost
+ * @property {number} weight item weight
+ * @property {number} amount amount of the item
+ * @property {number} bloodMagicDamage number of Bloodmagic damage the actor is receiving
  */
 export default class PhysicalItemTemplate extends SystemDataModel {
 
     /** @inheritDoc */
     static defineSchema() {
-        return {};
+        return this.mergeSchema( super.defineSchema(), {
+            price: new foundry.data.fields.SchemaField( {
+                value: new foundry.data.fields.NumberField( {
+                    required: true,
+                    nullable: false,
+                    min: 0,
+                    initial: 0,
+                    positive: true,
+                    label: "ED.Item.value"
+                } ),
+                coinage: foundry.data.fields.StringField( {
+                    required: true,
+                    nullable: false,
+                    min: 0,
+                    initial: 0,
+                    positive: true,
+                    label: "ED.Item.coinage"
+                } )
+            },
+            {
+                label: "ED.Item.price"
+            } ),
+
+
+            weight: new foundry.data.fields.NumberField( {
+                required: true,
+                nullable: false,
+                min: 0,
+                initial: 0,
+                positive: true,
+                label: "ED.Item.weight"
+            } ),
+            // availability types are Everyday, Average, Unusual, Rare, Very Rare, Unique
+            availability: new foundry.data.fields.StringField( {
+                required: true,
+                blank: false,
+                initial: "average",
+                label:  "ED.Item.availability"
+
+            } ),
+            amount: new foundry.data.fields.NumberField( {
+                required: true,
+                nullable: false,
+                min: 0,
+                initial: 0,
+                positive: true,
+                label: "ED.Item.amount"
+            } ),
+            bloodMagicDamage: new foundry.data.fields.NumberField( {
+                required: true,
+                nullable: false,
+                min: 0,
+                initial: 0,
+                positive: true,
+                label: "ED.Item.bloodMagicDamage"
+            } ),
+        } );
     }
 
     /* -------------------------------------------- */
@@ -15,8 +77,8 @@ export default class PhysicalItemTemplate extends SystemDataModel {
     /* -------------------------------------------- */
 
     /** @inheritDoc */
-    static migrateData(source) {
-        super.migrateData(source);
+    static migrateData( source ) {
+        super.migrateData( source );
         // specific migration functions
     }
 }
