1837448fff4b2126e1f31c0ed676993001e07d3a
Chris <christopherDick00@gmail.com>
2023-10-24 17:56:30 +0200

    Extend attributes data model for PCs

--- module/data/actor/pc.mjs
+++ module/data/actor/pc.mjs
@@ -14,51 +14,46 @@ export default class PcData extends NamegiverTemplate {
 
     /** @inheritDoc */
     static defineSchema() {
-        return this.mergeSchema( super.defineSchema(), {
-            attributes: new MappingField( new foundry.data.fields.SchemaField( {
-                initialValue: new foundry.data.fields.NumberField( {
-                    required: true,
-                    nullable: false,
-                    min: 1,
-                    step: 1,
-                    initial: 1,
-                    integer: true,
-                    positive: true
-                } ),
-                baseValue: new foundry.data.fields.NumberField( {
-                    required: true,
-                    nullable: false,
-                    min: 1,
-                    step: 1,
-                    initial: 1,
-                    integer: true,
-                    positive: true
-                } ),
-                value: new foundry.data.fields.NumberField( {
-                    required: true,
-                    nullable: false,
-                    min: 1,
-                    step: 1,
-                    initial: 1,
-                    integer: true,
-                    positive: true
-                } ),
-                timesIncreased: new foundry.data.fields.NumberField( {
-                    required: true,
-                    nullable: false,
-                    min: 0,
-                    max: 3,
-                    step: 1,
-                    initial: 0,
-                    integer: true,
-                    positive: true
-                } ),
-            } ), {
-                initialKeys: CONFIG.ED4E.attributes,
-                initialKeysOnly: true,
-                label: "ED.Attributes.attributes"
-            } )
+        const superSchema = super.defineSchema();
+        this.mergeSchema( superSchema.attributes.model.fields,  {
+            initialValue: new foundry.data.fields.NumberField( {
+                required: true,
+                nullable: false,
+                min: 1,
+                step: 1,
+                initial: 1,
+                integer: true,
+                positive: true
+            } ),
+            baseValue: new foundry.data.fields.NumberField( {
+                required: true,
+                nullable: false,
+                min: 1,
+                step: 1,
+                initial: 1,
+                integer: true,
+                positive: true
+            } ),
+            value: new foundry.data.fields.NumberField( {
+                required: true,
+                nullable: false,
+                min: 1,
+                step: 1,
+                initial: 1,
+                integer: true,
+                positive: true
+            } ),
+            timesIncreased: new foundry.data.fields.NumberField( {
+                required: true,
+                nullable: false,
+                min: 0,
+                max: 3,
+                step: 1,
+                initial: 0,
+                integer: true
+            } ),
         } );
+        return superSchema;
     }
 
     /* -------------------------------------------- */
