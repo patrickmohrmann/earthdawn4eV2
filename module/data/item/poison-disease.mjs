5eacbb7c1a74dec0063c065ff67ed5bc012eecd5
patrickmohrmann <patrick.mohrmann@outlook.de>
2023-10-25 23:31:10 +0200

    poison / disease

--- module/data/item/poison-disease.mjs
+++ module/data/item/poison-disease.mjs
@@ -2,12 +2,95 @@ import SystemDataModel from "../abstract.mjs";
 
 /**
  * Data model template with information on Poison and Disease items.
+ * @property {object} effect                      effect type
+ * @property {number} effect.damageStep           damage step
+ * @property {number} effect.paralysisStep        paralysis step
+ * @property {number} effect.debilitationStep     debilitation step
+ * @property {object} interval                    interval 
+ * @property {number} interval.totalEffects       total number of effects
+ * @property {number} interval.timeInBetween      time between effects
+ * @property {number} onsetTime                   after which time will the poison become effective
+ * @property {number} duration                    duration
+ * @property {string} activation            how the poison will be activated
+ * @property {boolean} death                deadly poison
  */
 export default class PoisonDiseaseData extends SystemDataModel {
 
     /** @inheritDoc */
     static defineSchema() {
-        return {};
+        return this.mergeSchema( super.defineSchema(), {
+            effect: new foundry.data.fields.SchemaField( {
+                damageStep: new foundry.data.fields.NumberField( {
+                    required: true,
+                    nullable: false,
+                    min: 0,
+                    initial: 0,
+                    label: "ED.Item.PoisonDisease.damageStep"
+                } ), 
+                paralysisStep: new foundry.data.fields.NumberField( {
+                    required: true,
+                    nullable: false,
+                    min: 0,
+                    initial: 0,
+                    label: "ED.Item.PoisonDisease.paralysisStep"
+                } ), 
+                debilitationStep: new foundry.data.fields.NumberField( {
+                    required: true,
+                    nullable: false,
+                    min: 0,
+                    initial: 0,
+                    label: "ED.Item.PoisonDisease.debilitationStep"
+                } ), 
+            },
+            {
+                label: "ED.Item.PoisonDisease.effect"
+            } ),
+            interval: new foundry.data.fields.SchemaField( {
+                totalEffects: new foundry.data.fields.NumberField( {
+                    required: true,
+                    nullable: false,
+                    min: 0,
+                    initial: 0,
+                    label: "ED.Item.PoisonDisease.totalEffects"
+                } ), 
+                timeInBetween: new foundry.data.fields.NumberField( {
+                    required: true,
+                    nullable: false,
+                    min: 0,
+                    initial: 0,
+                    label: "ED.Item.PoisonDisease.timeInBetween"
+                } ), 
+            },
+            {
+                label: "ED.Item.PoisonDisease.interval"
+            } ),
+            onsetTime: new foundry.data.fields.NumberField( {
+                required: true,
+                nullable: false,
+                min: 0,
+                initial: 0,
+                label: "ED.Item.PoisonDisease.onsetTime"
+            } ), 
+            duration: new foundry.data.fields.NumberField( {
+                required: true,
+                nullable: false,
+                min: 0,
+                initial: 0,
+                label: "ED.Item.PoisonDisease.duration"
+            } ), 
+            activation: new foundry.data.fields.StringField( {
+                required: true,
+                blank: false,
+                initial: "wound",
+                label: "ED.Item.PoisonDisease.activation"
+            } ),
+            death: new foundry.data.fields.BooleanField( {
+                required: true,
+                initial: false,
+                label: "ED.Item.PoisonDisease.death"
+            } ),
+            
+        } );
     }
 
     /* -------------------------------------------- */
@@ -15,8 +98,8 @@ export default class PoisonDiseaseData extends SystemDataModel {
     /* -------------------------------------------- */
 
     /** @inheritDoc */
-    static migrateData(source) {
-        super.migrateData(source);
+    static migrateData( source ) {
+        super.migrateData( source );
         // specific migration functions
     }
 }
