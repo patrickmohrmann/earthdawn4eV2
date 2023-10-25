993285ba4585e70d2659f381b03fc364c6676a4f
patrickmohrmann <patrick.mohrmann@outlook.de>
2023-10-25 11:09:12 +0200

    Update class.mjs

--- module/data/item/templates/class.mjs
+++ module/data/item/templates/class.mjs
@@ -2,12 +2,27 @@ import SystemDataModel from "../../abstract.mjs";
 
 /**
  * Data model template with information on "class"-like items: paths, disciplines, and questors.
+ * @property {number} level         either circle or rank of the class 
+ * @property {string} identifier    type of class
  */
 export default class ClassTemplate extends SystemDataModel{
 
     /** @inheritDoc */
     static defineSchema() {
-        return {};
+        return this.mergeSchema( super.defineSchema(), {
+            level: new foundry.data.fields.NumberField( {
+                required: true,
+                nullable: false,
+                min: 1,
+                initial: 1,
+                positive: true,
+            } ), 
+            identifier: new foundry.data.fields.StringField( {
+                required: true,
+                blank: false,
+                initial: "discipline",
+            } ), 
+        } );
     }
 
     /* -------------------------------------------- */
@@ -15,8 +30,8 @@ export default class ClassTemplate extends SystemDataModel{
     /* -------------------------------------------- */
 
     /** @inheritDoc */
-    static migrateData(source) {
-        super.migrateData(source);
+    static migrateData( source ) {
+        super.migrateData( source );
         // specific migration functions
     }
 }
