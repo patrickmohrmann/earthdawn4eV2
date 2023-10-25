71663c42c6a99a5cadceb20172a405f5694e8b23
patrickmohrmann <patrick.mohrmann@outlook.de>
2023-10-25 23:14:02 +0200

    Namegiver data model

--- module/data/item/namegiver.mjs
+++ module/data/item/namegiver.mjs
@@ -2,12 +2,115 @@ import SystemDataModel from "../abstract.mjs";
 
 /**
  * Data model template with information on namegiver items.
+ * @property {number} dexterityValue            dexterity value
+ * @property {number} strengthValue             strength value
+ * @property {number} constitutionValue         constitution value
+ * @property {number} perceptionValue           perception value
+ * @property {number} willpowerValue            willpower value
+ * @property {number} charismaValue             charisma value
+ * @property {number} karmamodifier             initiative value
+ * @property {object} movement                  movement group object
+ * @property {number} movement.walk             movement type walk modifications
+ * @property {number} movement.fly              movement type fly modifications
+ * @property {number} movement.swim             movement type swim modifications
+ * @property {number} movement.burrow           movement type burrow modifications
+ * @property {number} movement.climb            movement type climb modifications
  */
 export default class NamegiverData extends SystemDataModel{
 
     /** @inheritDoc */
     static defineSchema() {
-        return {};
+        return this.mergeSchema( super.defineSchema(), {
+            dexterityValue: new foundry.data.fields.NumberField( {
+                required: true,
+                nullable: false,
+                min: 1,
+                initial: 10,
+                label: "ED.Item.Namegiver.dexterityValue"
+            } ), 
+            strengthValue: new foundry.data.fields.NumberField( {
+                required: true,
+                nullable: false,
+                min: 1,
+                initial: 10,
+                label: "ED.Item.Namegiver.strengthValue"
+            } ), 
+            constitutionValue: new foundry.data.fields.NumberField( {
+                required: true,
+                nullable: false,
+                min: 1,
+                initial: 10,
+                label: "ED.Item.Namegiver.constitutionValue"
+            } ), 
+            perceptionValue: new foundry.data.fields.NumberField( {
+                required: true,
+                nullable: false,
+                min: 1,
+                initial: 10,
+                label: "ED.Item.Namegiver.perceptionValue"
+            } ), 
+            willpowerValue: new foundry.data.fields.NumberField( {
+                required: true,
+                nullable: false,
+                min: 1,
+                initial: 10,
+                label: "ED.Item.Namegiver.willpowerValue"
+            } ), 
+            charismaValue: new foundry.data.fields.NumberField( {
+                required: true,
+                nullable: false,
+                min: 1,
+                initial: 10,
+                label: "ED.Item.Namegiver.charismaValue"
+            } ), 
+            karmamodifier: new foundry.data.fields.NumberField( {
+                required: true,
+                nullable: false,
+                min: 0,
+                initial: 0,
+                label: "ED.Item.Namegiver.karmamodifier"
+            } ), 
+            movement: new foundry.data.fields.SchemaField( {
+                walk: new foundry.data.fields.NumberField( {
+                    required: true,
+                    nullable: false,
+                    min: 0,
+                    initial: 0,
+                    label: "ED.Item.Namegiver.walk"
+                } ),
+                fly: new foundry.data.fields.NumberField( {
+                    required: true,
+                    nullable: false,
+                    min: 0,
+                    initial: 0,
+                    label: "ED.Item.Namegiver.fly"
+                } ),
+                swim: new foundry.data.fields.NumberField( {
+                    required: true,
+                    nullable: false,
+                    min: 0,
+                    initial: 0,
+                    label: "ED.Item.Namegiver.swim"
+                } ),
+                burrow: new foundry.data.fields.NumberField( {
+                    required: true,
+                    nullable: false,
+                    min: 0,
+                    initial: 0,
+                    label: "ED.Item.Namegiver.burrow"
+                } ),
+                climb: new foundry.data.fields.NumberField( {
+                    required: true,
+                    nullable: false,
+                    min: 0,
+                    initial: 0,
+                    label: "ED.Item.Namegiver.climb"
+                } ),
+            },
+            {
+                label: "ED.Item.Namegiver.movement"
+            } ),
+        } );
     }
 
     /* -------------------------------------------- */
@@ -15,8 +118,8 @@ export default class NamegiverData extends SystemDataModel{
     /* -------------------------------------------- */
 
     /** @inheritDoc */
-    static migrateData(source) {
-        super.migrateData(source);
+    static migrateData( source ) {
+        super.migrateData( source );
         // specific migration functions
     }
 }
