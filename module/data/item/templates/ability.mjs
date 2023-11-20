9a80eb224facd28d77edd6aa52eda9c8db8da1b1
Chris <christopherDick00@gmail.com>
2023-11-20 23:24:45 +0100

    Add source field for types like talents and skills.

--- module/data/item/templates/ability.mjs
+++ module/data/item/templates/ability.mjs
@@ -1,12 +1,13 @@
 import SystemDataModel from "../../abstract.mjs";
+// import ClassTemplate from './class.mjs';
 
 /**
  * Data model template with information on Ability items.
- * @property {string} action action type 
+ * @property {string} action action type
  * @property {string} attribute attribute
  * @property {string} tier talent tier
- * @property {number} strain strain 
- * @property {number} level rank 
+ * @property {number} strain strain
+ * @property {number} level rank
  */
 export default class AbilityTemplate extends SystemDataModel {
     /** @inheritDoc */
@@ -19,33 +20,42 @@ export default class AbilityTemplate extends SystemDataModel {
                 initial: "standard",
                 label: "ED.Item.Ability.action"
             } ),
-            // TODO: do all actions have an attribute?
             attribute: new foundry.data.fields.StringField( {
-                required: true,
+                required: false,
                 nullable: true,
                 blank: true,
                 label: "ED.Item.Ability.attribute"
-            } ), 
-            tier: new foundry.data.fields.StringField( {
-                required: true,
-                nullable: false,
-                blank: false,
-                initial: "novice",
-                label: "ED.Item.Ability.tier"
-            } ), 
-            strain: new foundry.data.fields.NumberField( {
-                required: true,
-                nullable: false,
-                min: 0,
-                initial: 0,
-                label: "ED.Item.Ability.strain"
-            } ), 
+            } ),
+            source: new foundry.data.fields.SchemaField( {
+                    // TODO: figure out how ForeignDocumentFields work
+                    /* class: new foundry.data.fields.ForeignDocumentField( ClassTemplate, {
+                        initial: null,
+                        label: "ED.Item.Class.source"
+                    } ), */
+                    tier: new foundry.data.fields.StringField( {
+                        nullable: false,
+                        blank: false,
+                        initial: "novice",
+                        label: "ED.Item.Ability.tier"
+                    } ),
+                },
+                {
+                    required: false
+                }
+            ),
             level: new foundry.data.fields.NumberField( {
                 required: true,
                 nullable: false,
                 min: 0,
                 initial: 0,
                 label: "ED.Item.Ability.rank"
+            } ),
+            strain: new foundry.data.fields.NumberField( {
+                required: true,
+                nullable: false,
+                min: 0,
+                initial: 0,
+                label: "ED.Item.Ability.strain"
             } ),
         } );
     }
