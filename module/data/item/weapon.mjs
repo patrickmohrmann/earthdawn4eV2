ab617724192f01b868973706f19ec6b17dbf8e3e
patrickmohrmann <patrick.mohrmann@outlook.de>
2023-12-17 00:17:34 +0100

    schemafield for weapon damage
    #297

--- module/data/item/weapon.mjs
+++ module/data/item/weapon.mjs
@@ -4,8 +4,9 @@ import ItemDescriptionTemplate from "./templates/item-description.mjs";
 /**
  * Data model template with information on weapon items.
  * @property {string} weaponType            type of weapon
- * @property {string} damageAttribute       base attribute used for damage
- * @property {number} damageBaseStep        weapon basic damage step
+ * @property {object} damage                damage object
+ * @property {string} damage.attribute       base attribute used for damage
+ * @property {number} damage.baseStep        weapon basic damage step
  * @property {number} size                  weapon size 1-7
  * @property {number} strengthMinimum       strength minimum to use without penalty
  * @property {number} dexterityMinimum      dexterity minimum to use without penalty
@@ -28,22 +29,23 @@ export default class WeaponData extends PhysicalItemTemplate.mixin(
                 label: "ED.Item.Weapon.Label.weaponType",
                 hint: "ED.Item.Weapon.Hint.weaponType"
             } ), 
-            // @Chris kann ich hier bei initial auf die Config verweisen?
-            damageAttribute: new foundry.data.fields.StringField( {
-                required: true,
-                nullable: false,
-                initial: "strength",
-                label: "ED.Item.Weapon.Label.damageAttribute",
-                hint: "ED.Item.Weapon.Hint.damageAttribute"
-            } ),
-            damageBaseStep: new foundry.data.fields.NumberField( {
-                required: true,
-                nullable: false,
-                min: 0,
-                initial: 0,
-                integer: true,
-                label: "ED.Item.Weapon.Label.damageBaseStep",
-                hint: "ED.Item.Weapon.Hint.damageBaseStep"
+            damage: new foundry.data.fields.SchemaField( {
+                attribute: new foundry.data.fields.StringField( {
+                    required: true,
+                    nullable: false,
+                    initial: "strength",
+                    label: "ED.Item.Weapon.Label.damageAttribute",
+                    hint: "ED.Item.Weapon.Hint.damageAttribute"
+                } ),
+                baseStep: new foundry.data.fields.NumberField( {
+                    required: true,
+                    nullable: false,
+                    min: 0,
+                    initial: 0,
+                    integer: true,
+                    label: "ED.Item.Weapon.Label.damageBaseStep",
+                    hint: "ED.Item.Weapon.Hint.damageBaseStep"
+                } ),
             } ),
             size: new foundry.data.fields.NumberField( {
                 required: true,
