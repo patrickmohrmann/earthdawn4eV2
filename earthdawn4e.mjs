c736b04f72b6f0abc076cb7a742d71ebebed5a44
Chris <christopherDick00@gmail.com>
2023-12-19 14:19:27 +0100

    Add pre-filled character actor for debugging

--- earthdawn4e.mjs
+++ earthdawn4e.mjs
@@ -27,13 +27,13 @@ import * as utils from "./module/utils.mjs";
 /* -------------------------------------------- */
 
 globalThis.ed4e = {
-    applications,
-    canvas,
-    config: ED4E,
-    dataModels,
-    dice,
-    documents,
-    utils
+  applications,
+  canvas,
+  config: ED4E,
+  dataModels,
+  dice,
+  documents,
+  utils
 };
 
 /* -------------------------------------------- */
@@ -42,91 +42,117 @@ globalThis.ed4e = {
 
 
 Hooks.once( "init", () => {
-    globalThis.ed4e = game.ed4e = Object.assign( game.system, globalThis.ed4e );
-    console.log( "ED4e | Initializing the ED4e Game System" );
+  globalThis.ed4e = game.ed4e = Object.assign( game.system, globalThis.ed4e );
+  console.log( "ED4e | Initializing the ED4e Game System" );
 
-    // record configuration values
-    CONFIG.ED4E = ED4E;
-    CONFIG.Actor.documentClass = documents.ActorEd;
-    CONFIG.Item.documentClass = documents.ItemEd;
+  // record configuration values
+  CONFIG.ED4E = ED4E;
+  CONFIG.Actor.documentClass = documents.ActorEd;
+  CONFIG.Item.documentClass = documents.ItemEd;
 
-    // Register System Settings
-    registerSystemSettings();
+  // Register System Settings
+  registerSystemSettings();
 
-    // Register Handlebars Helper
-    registerHandlebarHelpers();
+  // Register Handlebars Helper
+  registerHandlebarHelpers();
 
-    // Hook up system data types
-    CONFIG.Actor.dataModels = dataModels.actor.config;
-    CONFIG.Item.dataModels = dataModels.item.config;
+  // Hook up system data types
+  CONFIG.Actor.dataModels = dataModels.actor.config;
+  CONFIG.Item.dataModels = dataModels.item.config;
 
-    // Register sheet application classes
-    Actors.unregisterSheet( "core", ActorSheet );
-    Actors.registerSheet( "earthdawn4e", applications.actor.ActorSheetEdCharacter, {
-        types: ["character"],
-        makeDefault: true
-    } );
-    Items.unregisterSheet( "core", ItemSheet );
-    Items.registerSheet( "earthdawn4e", applications.item.ItemSheetEd, {
-        makeDefault: true
-    } );
+  // Register sheet application classes
+  Actors.unregisterSheet( "core", ActorSheet );
+  Actors.registerSheet( "earthdawn4e", applications.actor.ActorSheetEdCharacter, {
+    types: ["character"],
+    makeDefault: true
+  } );
+  Items.unregisterSheet( "core", ItemSheet );
+  Items.registerSheet( "earthdawn4e", applications.item.ItemSheetEd, {
+    makeDefault: true
+  } );
 
-    // Preload Handlebars templates.
-    utils.preloadHandlebarsTemplates();
+  // Preload Handlebars templates.
+  utils.preloadHandlebarsTemplates();
 
 } );
 
 Hooks.once( 'ready', async () => {
-    // Debug stuff
 
-    game.folders.forEach( ( value, key, map ) => {
-        if ( value.flags.deleteOnStartup ) value.delete();
+  /* -------------------------------------------- */
+  /*  Debug Documents                             */
+  /* -------------------------------------------- */
+
+  // Create on document for each type
+
+  game.folders.forEach( ( value, key, map ) => {
+    if ( value.flags.deleteOnStartup ) value.delete();
+  } );
+  game.items.forEach( ( value, key, map ) => {
+    if ( value.flags.deleteOnStartup ) value.delete();
+  } );
+  game.actors.forEach( ( value, key, map ) => {
+    if ( value.flags.deleteOnStartup ) value.delete();
+  } );
+
+  const actorFolder = await Folder.create(
+    {
+      "name": "DebugActors",
+      "type": "Actor",
+      "description": "<p>Contains data created for debugging purposes</p>",
+      "color": "#efdaca",
+      "flags": { deleteOnStartup: true }
     } );
-    game.items.forEach( ( value, key, map ) => {
-        if ( value.flags.deleteOnStartup ) value.delete();
+  const itemFolder = await Folder.create(
+    {
+      "name": "DebugItems",
+      "type": "Item",
+      "description": "<p>Contains data created for debugging purposes</p>",
+      "color": "#efdaca",
+      "flags": { deleteOnStartup: true }
     } );
-    game.actors.forEach( ( value, key, map ) => {
-        if ( value.flags.deleteOnStartup ) value.delete();
-    } );
-
-    const actorFolder = await Folder.create(
-      {
-          "name": "DebugActors",
-          "type": "Actor",
-          "description": "<p>Contains data created for debugging purposes</p>",
-          "color": "#efdaca",
-          "flags": { deleteOnStartup: true }
-      } );
-    const itemFolder = await Folder.create(
-      {
-          "name": "DebugItems",
-          "type": "Item",
-          "description": "<p>Contains data created for debugging purposes</p>",
-          "color": "#efdaca",
-          "flags": { deleteOnStartup: true }
-      } );
 
 
-    const createdActors = [];
-    const createdItems = [];
-    for ( const actorType of Object.keys( CONFIG.Actor.dataModels ) ) {
-        createdActors.push(
-          ed4e.documents.ActorEd.create( {
-              name: actorType,
-              type: actorType,
-              folder: actorFolder.id,
-              flags: { deleteOnStartup: true }
-          } ),
-        );
-    }
-    for ( const itemType of Object.keys( CONFIG.Item.dataModels ) ) {
-        createdItems.push(
-          ed4e.documents.ItemEd.create( {
-              name: itemType,
-              type: itemType,
-              folder: itemFolder.id,
-              flags: { deleteOnStartup: true }
-          } ),
-        );
-    }
+  const createdActors = {};
+  const createdItems = {};
+  for ( const actorType of Object.keys( CONFIG.Actor.dataModels ) ) {
+    createdActors[actorType] = await
+      ed4e.documents.ActorEd.create( {
+          name: actorType,
+          type: actorType,
+          folder: actorFolder.id,
+          flags: { deleteOnStartup: true }
+        }
+      );
+  }
+  for ( const itemType of Object.keys( CONFIG.Item.dataModels ) ) {
+    createdItems[itemType] = await
+      ed4e.documents.ItemEd.create( {
+          name: itemType,
+          type: itemType,
+          folder: itemFolder.id,
+          flags: { deleteOnStartup: true }
+        }
+      );
+  }
+
+  console.debug( "Created actors: ", createdActors );
+  console.debug( "Created items: ", createdItems );
+  // Prepare documents
+
+  const character = createdActors["character"];
+  await character.createEmbeddedDocuments(
+    "Item",
+    [
+      createdItems.armor.toObject(),
+      createdItems.devotion.toObject(),
+      createdItems.discipline.toObject(),
+      createdItems.equipment.toObject(),
+      createdItems.namegiver.toObject(),
+      createdItems.questor.toObject(),
+      createdItems.shield.toObject(),
+      createdItems.skill.toObject(),
+      createdItems.talent.toObject(),
+      createdItems.weapon.toObject(),
+    ]
+  )
 } );
