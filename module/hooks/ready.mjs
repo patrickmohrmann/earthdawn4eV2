96328f3ff67cb484ba59912eba997b13431482d1
patrickmohrmann <patrick.mohrmann@outlook.de>
2024-01-05 10:09:37 +0100

    Update ready.mjs
    eslint corrections

--- module/hooks/ready.mjs
+++ module/hooks/ready.mjs
@@ -1,5 +1,8 @@
+/**
+ * TODO Chris 
+ */
 export default function () {
-    Hooks.once( 'ready', async () => {
+    Hooks.once( "ready", async () => {
       /* -------------------------------------------- */
       /*  Debug Documents                             */
       /* -------------------------------------------- */
@@ -7,57 +10,60 @@ export default function () {
     } );
 }
 
+/**
+ * Creation of actors and items for debugging purposes
+ */
 async function _createDebugDocuments() {
     // Create on document for each type
 
-    game.folders.forEach((value, key, map) => {
-        if (value.flags.deleteOnStartup) value.delete();
-    });
-    game.items.forEach((value, key, map) => {
-        if (value.flags.deleteOnStartup) value.delete();
-    });
-    game.actors.forEach((value, key, map) => {
-        if (value.flags.deleteOnStartup) value.delete();
-    });
+    game.folders.forEach( ( value, key, map ) => {
+        if ( value.flags.deleteOnStartup ) value.delete();
+    } );
+    game.items.forEach( ( value, key, map ) => {
+        if ( value.flags.deleteOnStartup ) value.delete();
+    } );
+    game.actors.forEach( ( value, key, map ) => {
+        if ( value.flags.deleteOnStartup ) value.delete();
+    } );
 
-    const actorFolder = await Folder.create({
-        name: 'DebugActors',
-        type: 'Actor',
-        description: '<p>Contains data created for debugging purposes</p>',
-        color: '#efdaca',
+    const actorFolder = await Folder.create( {
+        name: "DebugActors",
+        type: "Actor",
+        description: "<p>Contains data created for debugging purposes</p>",
+        color: "#efdaca",
         flags: { deleteOnStartup: true },
-    });
-    const itemFolder = await Folder.create({
-        name: 'DebugItems',
-        type: 'Item',
-        description: '<p>Contains data created for debugging purposes</p>',
-        color: '#efdaca',
+    } );
+    const itemFolder = await Folder.create( {
+        name: "DebugItems",
+        type: "Item",
+        description: "<p>Contains data created for debugging purposes</p>",
+        color: "#efdaca",
         flags: { deleteOnStartup: true },
-    });
+    } );
 
     const createdActors = {};
     const createdItems = {};
-    for (const actorType of Object.keys(CONFIG.Actor.dataModels)) {
-        createdActors[actorType] = await ed4e.documents.ActorEd.create({
+    for ( const actorType of Object.keys( CONFIG.Actor.dataModels ) ) {
+        createdActors[actorType] = await ed4e.documents.ActorEd.create( {
             name: actorType,
             type: actorType,
             folder: actorFolder.id,
             flags: { deleteOnStartup: true },
-        });
+        } );
     }
-    for (const itemType of Object.keys(CONFIG.Item.dataModels)) {
-        createdItems[itemType] = await ed4e.documents.ItemEd.create({
+    for ( const itemType of Object.keys( CONFIG.Item.dataModels ) ) {
+        createdItems[itemType] = await ed4e.documents.ItemEd.create( {
             name: itemType,
             type: itemType,
             folder: itemFolder.id,
             flags: { deleteOnStartup: true },
-        });
+        } );
     }
 
     // Prepare documents
 
-    const character = createdActors['character'];
-    await character.createEmbeddedDocuments('Item', [
+    const character = createdActors["character"];
+    await character.createEmbeddedDocuments( "Item", [
         createdItems.armor.toObject(),
         createdItems.devotion.toObject(),
         createdItems.discipline.toObject(),
@@ -68,5 +74,5 @@ async function _createDebugDocuments() {
         createdItems.skill.toObject(),
         createdItems.talent.toObject(),
         createdItems.weapon.toObject(),
-    ]);
+    ] );
 }
