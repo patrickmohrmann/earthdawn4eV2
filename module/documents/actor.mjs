ad98cd7d0a5821895734083da7892445304caad1
Chris <christopherDick00@gmail.com>
2024-01-25 17:57:01 +0100

    Remove superfluous edRollOptions property

--- module/documents/actor.mjs
+++ module/documents/actor.mjs
@@ -119,10 +119,10 @@ export default class ActorEd extends Actor {
       return;
     }
     // Check if this uses karma or strain at all
-    this.takeDamage(roll.edRollOptions.strain.total, 'standard');
+    this.takeDamage(roll.options.strain.total, 'standard');
     if (
-      !this.#useResource('karma', roll.edRollOptions.karma.pointsUsed) ||
-      !this.#useResource('devotion', roll.edRollOptions.devotion.pointsUsed)
+      !this.#useResource('karma', roll.options.karma.pointsUsed) ||
+      !this.#useResource('devotion', roll.options.devotion.pointsUsed)
     ) {
       ui.notifications.warn('Localize: Not enough karma or devotion. Used all that was available.');
     }
