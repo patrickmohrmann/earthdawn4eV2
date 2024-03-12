f5f4135c2f0b6f24260a404d65122692f5f260da
patrickmohrmann <patrick.mohrmann@outlook.de>
2024-03-12 09:06:56 +0100

    broken from char gen merge

--- module/applications/actor/base-sheet.mjs
+++ module/applications/actor/base-sheet.mjs
@@ -73,15 +73,14 @@ export default class ActorSheetEd extends ActorSheet {
 
     // Character Generation
     html.find( ".character-generation" ).click( this._onCharacterGeneration.bind( this ) );
+
+    // Legend point History (Earned)
+    html.find( ".legend-point__history--earned" ).click( this._onLegendPointHistoryEarned.bind( this ) );
   }
 
   _onCharacterGeneration( event ) {
     event.preventDefault();
     this.actor.characterGeneration( );
-
-    // Legend point History (Earned)
-    html.find( ".legend-point__history--earned" ).click( this._onLegendPointHistoryEarned.bind( this ) );
-
   }
 
   /**
