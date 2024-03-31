caacba1fcce9bd6413cb9b6e4e6d43aa7a3bfdcf
Chris <christopherDick00@gmail.com>
2024-03-31 21:12:11 +0200

    Fixes #496

--- module/dice/ed-roll.mjs
+++ module/dice/ed-roll.mjs
@@ -62,6 +62,15 @@ export default class EdRoll extends Roll {
   /* -------------------------------------------- */
 
   /**
+   * Return the total result of the Roll expression if it has been evaluated. This
+   * always evaluates to at least 1.
+   * @type {number}
+   */
+  get total() {
+    return  Math.max( super.total, 1 );
+  }
+
+  /**
    * Is this roll a valid Earthdawn test?
    * @type {boolean}
    */
