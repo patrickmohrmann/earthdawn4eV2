3c8d7c8bf37be8a4549a7bcf8a418875e0ca52e0
Chris <christopherDick00@gmail.com>
2024-05-11 15:36:13 +0200

    Add getter for magic type of casting disciplines

--- module/data/actor/templates/namegiver.mjs
+++ module/data/actor/templates/namegiver.mjs
@@ -11,6 +11,16 @@ export default class NamegiverTemplate extends SentientTemplate {
         return super.defineSchema();
     }
 
+    /**
+     * Gets the type of magic of the first thread weaving talent encountered.
+     * @type {string} The type of thread weaving magic as defined in {@link ED4E.spellcastingTypes},
+     * or an empty string if there is no corresponding talent.
+     */
+    get magicType() {
+        const threadWeavingTalents = this.parent.items.filter( item => item.system?.magic?.threadWeaving );
+        return threadWeavingTalents[0]?.system.magic.magicType ?? "";
+    }
+
     /* -------------------------------------------- */
     /*  Migrations                                  */
     /* -------------------------------------------- */
