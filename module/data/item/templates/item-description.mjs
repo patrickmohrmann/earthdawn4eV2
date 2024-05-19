051026773b1c4e7af4cae22f0a10efa7d2043d32
Chris <christopherDick00@gmail.com>
2024-05-19 21:04:10 +0200

    Add edid field to items
    Description is mixed in everywhere

--- module/data/item/templates/item-description.mjs
+++ module/data/item/templates/item-description.mjs
@@ -1,3 +1,6 @@
+import ED4E from "../../../config.mjs";
+import { validateEdid } from "../../../utils.mjs";
+
 /**
  * Data model template with item description
  * @mixin
@@ -14,7 +17,15 @@ export default class ItemDescriptionTemplate extends foundry.abstract.DataModel 
                     nullable: true, 
                     label: "ED.Description"
                 } ), 
-            } )
+            } ),
+            edid: new fields.StringField( {
+                initial: ED4E.reserved_edid.DEFAULT,
+                blank: false,
+                required: true,
+                validate: ( value, _ ) => validateEdid( value ),
+                label: "X.ED-ID",
+                hints: "X.Earthdawn ID to identify an item without considering the name",
+            } ),
         };
     }
 
