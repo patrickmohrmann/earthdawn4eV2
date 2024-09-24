e68a9bb43edb28ca7af57c4a6f230974fa723443
Patrick Mohrmann <patrick.mohrmann@koerber.com>
2024-09-24 16:39:59 +0200

    lint

--- module/data/item/armor.mjs
+++ module/data/item/armor.mjs
@@ -13,98 +13,98 @@ import ItemDescriptionTemplate from "./templates/item-description.mjs";
  * @property {number} piecemealArmor.piecemealArmorSize           piecemeal Armor size value can be 1, 2 or 3
  */
 export default class ArmorData extends PhysicalItemTemplate.mixin(
-    ItemDescriptionTemplate
+  ItemDescriptionTemplate
 ) {
 
-    /** @inheritDoc */
-    static defineSchema() {
-        const fields = foundry.data.fields;
-        return this.mergeSchema( super.defineSchema(), {
-            physical: new fields.SchemaField( {
-                armor: new fields.NumberField( {
-                required: true,
-                nullable: false,
-                min:      0,
-                initial:  0,
-                integer:  true,
-                label:    "ED.Item.Armor.physicalArmor"
-                } ), 
-                forgeBonus: new fields.NumberField( {
-                    required: true,
-                    nullable: false,
-                    min:      0,
-                    initial:  0,
-                    integer:  true,
-                    label:    "ED.Item.Armor.forgeBonusPhysical"
-                } ),
-            } ),
-            mystical: new fields.SchemaField( {
-                armor: new fields.NumberField( {
-                    required: true,
-                    nullable: false,
-                    min:      0,
-                    initial:  0,
-                    integer:  true,
-                    label:    "ED.Item.Armor.mysticalArmor"
-                } ),
-                forgeBonus: new fields.NumberField( {
-                    required: true,
-                    nullable: false,
-                    min:      0,
-                    initial:  0,
-                    integer:  true,
-                    label:    "ED.Item.Armor.forgeBonusMystical"
-                } ),
-            } ),
-            initiativePenalty: new fields.NumberField( {
-                required: true,
-                nullable: false,
-                min:      0,
-                initial:  0,
-                integer:  true,
-                label:    "ED.Item.Armor.initiativePenalty"
-            } ),
-            isLivingArmor: new fields.BooleanField( {
-                required: true,
-                initial:  false,
-                label:    "ED.Data.Item.Labels.livingArmor",
-                hint:     "ED.Data.Item.Hints.livingArmor"
-            } ),
-            piecemealArmor: new fields.SchemaField( {
-                selector: new fields.BooleanField( {
-                    required: true,
-                    initial:  false,
-                    label:    "ED.Item.Armor.piecemealArmor"
-                } ),
-                size: new fields.NumberField( {
-                    required: true,
-                    nullable: false,
-                    min:      0,
-                    max:      3,
-                    initial:  0,
-                    integer:  true,
-                    label:    "ED.Item.Armor.piecemealArmorSize"
-                } ),
-            }, {
-                required: true,
-                nullable: false,
-                label:    "ED.Data.Item.Labels.piecemealArmor",
-                hint:     "ED.Data.Item.Hints.piecemealArmor",
-            } ),
-        } );
-    }
+  /** @inheritDoc */
+  static defineSchema() {
+      const fields = foundry.data.fields;
+      return this.mergeSchema( super.defineSchema(), {
+        physical: new fields.SchemaField( {
+          armor: new fields.NumberField( {
+          required: true,
+          nullable: false,
+          min:      0,
+          initial:  0,
+          integer:  true,
+          label:    "ED.Item.Armor.physicalArmor"
+          } ), 
+          forgeBonus: new fields.NumberField( {
+              required: true,
+              nullable: false,
+              min:      0,
+              initial:  0,
+              integer:  true,
+              label:    "ED.Item.Armor.forgeBonusPhysical"
+          } ),
+        } ),
+        mystical: new fields.SchemaField( {
+          armor: new fields.NumberField( {
+              required: true,
+              nullable: false,
+              min:      0,
+              initial:  0,
+              integer:  true,
+              label:    "ED.Item.Armor.mysticalArmor"
+          } ),
+          forgeBonus: new fields.NumberField( {
+              required: true,
+              nullable: false,
+              min:      0,
+              initial:  0,
+              integer:  true,
+              label:    "ED.Item.Armor.forgeBonusMystical"
+          } ),
+        } ),
+        initiativePenalty: new fields.NumberField( {
+          required: true,
+          nullable: false,
+          min:      0,
+          initial:  0,
+          integer:  true,
+          label:    "ED.Item.Armor.initiativePenalty"
+        } ),
+        isLivingArmor: new fields.BooleanField( {
+          required: true,
+          initial:  false,
+          label:    "ED.Data.Item.Labels.livingArmor",
+          hint:     "ED.Data.Item.Hints.livingArmor"
+        } ),
+        piecemealArmor: new fields.SchemaField( {
+          selector: new fields.BooleanField( {
+          required: true,
+          initial:  false,
+          label:    "ED.Item.Armor.piecemealArmor"
+          } ),
+          size: new fields.NumberField( {
+              required: true,
+              nullable: false,
+              min:      0,
+              max:      3,
+              initial:  0,
+              integer:  true,
+              label:    "ED.Item.Armor.piecemealArmorSize"
+          } ),
+        }, {
+          required: true,
+          nullable: false,
+          label:    "ED.Data.Item.Labels.piecemealArmor",
+          hint:     "ED.Data.Item.Hints.piecemealArmor",
+        } ),
+      } );
+  }
 
-    /* -------------------------------------------- */
-    /*  Getters                                     */
-    /* -------------------------------------------- */
+  /* -------------------------------------------- */
+  /*  Getters                                 */
+  /* -------------------------------------------- */
 
-    /* -------------------------------------------- */
-    /*  Migrations                                  */
-    /* -------------------------------------------- */
+  /* -------------------------------------------- */
+  /*  Migrations                                  */
+  /* -------------------------------------------- */
 
-    /** @inheritDoc */
-    static migrateData( source ) {
-        super.migrateData( source );
-        // specific migration functions
-    }
+  /** @inheritDoc */
+  static migrateData( source ) {
+      super.migrateData( source );
+      // specific migration functions
+  }
 }
--- module/data/item/shield.mjs
+++ module/data/item/shield.mjs
@@ -3,82 +3,82 @@ import ItemDescriptionTemplate from "./templates/item-description.mjs";
 
 /**
  * Data model template with information on shield items.
- * @property {number} defenseBonusPhysical      physical defense bonus
- * @property {number} defenseBonusMystical      mystical defense bonus
- * @property {number} initiativePenalty         initiative penalty
- * @property {number} shatterThreshold          shatter threshold
- * @property {boolean} broken                   broken condition
+ * @property {number} defenseBonusPhysical    physical defense bonus
+ * @property {number} defenseBonusMystical    mystical defense bonus
+ * @property {number} initiativePenalty     initiative penalty
+ * @property {number} shatterThreshold      shatter threshold
+ * @property {boolean} broken           broken condition
  */
 export default class ShieldData extends PhysicalItemTemplate.mixin(
-    ItemDescriptionTemplate
+  ItemDescriptionTemplate
 ) {
-    /** @inheritDoc */
-    static defineSchema() {
-        const fields = foundry.data.fields;
-        return this.mergeSchema( super.defineSchema(), {
-            defenseBonus: new fields.SchemaField( {
-                physical: new fields.NumberField( {
-                    required: true,
-                    nullable: false,
-                    min: 0,
-                    initial: 0,
-                    integer: true,
-                    hint: "ED.Item.Shield.Label.defenseBonusPhysical",
-                    label: "ED.Item.Shield.Hint.defenseBonusPhysical"
-                } ),
-                mystical: new fields.NumberField( {
-                    required: true,
-                    nullable: false,
-                    min: 0,
-                    initial: 0,
-                    integer: true,
-                    hint: "ED.Item.Shield.Label.defenseBonusMystical",
-                    label: "ED.Item.Shield.Hint.defenseBonusMystical"
-                } ),
-            } ),
-            initiativePenalty: new fields.NumberField( {
-                required: true,
-                nullable: false,
-                min: 0,
-                initial: 0,
-                integer: true,
-                hint: "ED.Item.Shield.Label.initiativePenalty",
-                label: "ED.Item.Shield.Hint.initiativePenalty"
-            } ),
-            shatterThreshold: new fields.NumberField( {
-                required: true,
-                nullable: false,
-                min: 0,
-                initial: 0,
-                integer: true,
-                hint: "ED.Item.Shield.Label.shatterThreshold",
-                label: "ED.Item.Shield.Hint.shatterThreshold"
-            } ),
-            broken: new fields.BooleanField( {
-                required: true,
-                initial: false,
-                label: "ED.Item.Shield.Label.broken"
-            } ),
-            isLivingArmor: new fields.BooleanField( {
-                required: true,
-                initial:  false,
-                label:    "ED.Data.Item.Labels.livingArmor",
-                hint:     "ED.Data.Item.Hints.livingArmor"
-            } ),
-            bowUsage: new fields.BooleanField( {
-                required: true,
-                initial: false,
-                label: "ED.Item.Shield.Label.bowUsage"
-            } ),
-    } );
-    }
-    /* -------------------------------------------- */
-    /*  Migrations                                  */
-    /* -------------------------------------------- */
+  /** @inheritDoc */
+  static defineSchema() {
+    const fields = foundry.data.fields;
+    return this.mergeSchema( super.defineSchema(), {
+      defenseBonus: new fields.SchemaField( {
+        physical: new fields.NumberField( {
+          required: true,
+          nullable: false,
+          min:      0,
+          initial:  0,
+          integer:  true,
+          hint:     "ED.Item.Shield.Label.defenseBonusPhysical",
+          label:    "ED.Item.Shield.Hint.defenseBonusPhysical"
+        } ),
+        mystical: new fields.NumberField( {
+          required: true,
+          nullable: false,
+          min:      0,
+          initial:  0,
+          integer:  true,
+          hint:     "ED.Item.Shield.Label.defenseBonusMystical",
+          label:    "ED.Item.Shield.Hint.defenseBonusMystical"
+        } ),
+      } ),
+      initiativePenalty: new fields.NumberField( {
+        required: true,
+        nullable: false,
+        min:      0,
+        initial:  0,
+        integer:  true,
+        hint:     "ED.Item.Shield.Label.initiativePenalty",
+        label:    "ED.Item.Shield.Hint.initiativePenalty"
+      } ),
+      shatterThreshold: new fields.NumberField( {
+        required: true,
+        nullable: false,
+        min:      0,
+        initial:  0,
+        integer:  true,
+        hint:     "ED.Item.Shield.Label.shatterThreshold",
+        label:    "ED.Item.Shield.Hint.shatterThreshold"
+      } ),
+      broken: new fields.BooleanField( {
+        required: true,
+        initial:  false,
+        label:    "ED.Item.Shield.Label.broken"
+      } ),
+      isLivingArmor: new fields.BooleanField( {
+        required: true,
+        initial:  false,
+        label:    "ED.Data.Item.Labels.livingArmor",
+        hint:     "ED.Data.Item.Hints.livingArmor"
+      } ),
+      bowUsage: new fields.BooleanField( {
+        required: true,
+        initial:  false,
+        label:    "ED.Item.Shield.Label.bowUsage"
+      } ),
+  } );
+  }
+  /* -------------------------------------------- */
+  /*  Migrations                  */
+  /* -------------------------------------------- */
 
-    /** @inheritDoc */
-    static migrateData( source ) {
-        super.migrateData( source );
-        // specific migration functions
-    }
+  /** @inheritDoc */
+  static migrateData( source ) {
+    super.migrateData( source );
+    // specific migration functions
+  }
 }
